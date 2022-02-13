1df2d8ae5a8fd15ed7d7d17a2aaf24a2fb25eeb9
p-le <phu.le.dev@gmail.com>
2022-02-13 08:37:30 +0000

    add deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+IMAGE_NAME="todoapp-backend"
+IMAGE_TAG="1.0.7"
+GCP_PROJECT_ID="phu-le-it"
+GCP_REGION="asia-northeast1"
+GCP_IMAGE_REPOSITORY="todoapp-repository"
+
+docker image build -t ${IMAGE_NAME}:dev .
+docker image tag ${IMAGE_NAME}:dev ${GCP_REGION}-docker.pkg.dev/${GCP_PROJECT_ID}/${GCP_IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
+docker image push ${GCP_REGION}-docker.pkg.dev/${GCP_PROJECT_ID}/${GCP_IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
